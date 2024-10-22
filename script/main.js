1f3bd8bff97d516218610d9b48115b316dbdd5ee
Simon <simontunje@hotmail.com>
2024-10-22 22:39:53 +0200

    done

--- script/main.js
+++ script/main.js
@@ -1,7 +1,26 @@
 window.onload = function() {
-    let price;
-    price = prompt("Enter your price!",500);
-    parseFloat(price);
-    console.log("Your price: " + (price * 0.9))
-    console.log("You saved: " + (price * 0.1))
+    let user;
+    let pass;
+    let check;
+    user = prompt("Enter your username:");
+    pass = prompt("Enter your password:");
+    if(pass.includes(user) == true)
+        {
+            check = false;
+        }
+    else if(pass.includes(" ") == true)
+        {
+            check = false;
+        }
+    else if(pass.length < 8)
+        {
+            check = false;
+        }
+    else
+        {
+            check = true;   
+        }
+    console.log("Username: "+user);
+    console.log("password: "+pass);
+    console.log("validation: "+check);
 };
